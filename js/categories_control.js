ead294a2d72bda13d10bbe87189d22c10bca9104
LoKi <Kirill.Likhodedov@gmail.com>
2009-07-12 22:48:58 +0400

    [Refactor] JQuery and separate css in categories control

--- js/categories_control.js
+++ js/categories_control.js
@@ -6,25 +6,22 @@ function CategoriesControl(categories) {
 CategoriesControl.prototype = new GControl();
 
 CategoriesControl.prototype.initialize = function(map) {
-  var container = new$('div');
+  var container = new$$('div');
 
-  this.selectedDiv = this.createCategoryElement(container, 'Все', 'all');
-  this.setSelectedStyle(this.selectedDiv);
+  this.selectedDiv = this.createCategoryElement(container, 'Все', 'all')
+  this.selectedDiv.addClass('catControlSelectedButton');
   for (var i = 0; i < this.categories.length; i++) {
     this.createCategoryElement(container, this.categories[i].name, this.categories[i].key);
   }
   
-  map.getContainer().appendChild(container);
-  return container;
+  map.getContainer().appendChild(container.context);
+  return container.context;
 }
 
 CategoriesControl.prototype.createCategoryElement = function(container, name, key) {
-  var div = new$("span");
-  this.setButtonStyle(div);
-  container.appendChild(div);
-  div.appendChild(document.createTextNode(name));
+  var div = new$$('span').addClass('catControlButton').text(name).appendTo(container);
   var outer = this;
-  GEvent.addDomListener(div, "click", function() {
+  GEvent.addDomListener(div.context, 'click', function() {
     var points = Event.CURRENT.getPointsByCategoryGroup(key);
     
     MAP.clear();
@@ -33,9 +30,9 @@ CategoriesControl.prototype.createCategoryElement = function(container, name, ke
     CheckpointsList.instance.clear();
     CheckpointsList.instance.print(points);
     
-    outer.setButtonStyle(outer.selectedDiv);
+    outer.selectedDiv.removeClass('catControlSelectedButton');
     outer.selectedDiv = div;
-    outer.setSelectedStyle(outer.selectedDiv);
+    div.addClass('catControlSelectedButton');
   });
   return div;
 }
@@ -43,22 +40,3 @@ CategoriesControl.prototype.createCategoryElement = function(container, name, ke
 CategoriesControl.prototype.getDefaultPosition = function() {
   return new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(70, 10));
 }
-
-CategoriesControl.prototype.setButtonStyle = function(button) {
-  button.style.color = "darkblue";
-  button.style.backgroundColor = "white";
-  button.style.fontWeight = 'normal';
-  button.style.fontSize = '90%';
-  button.style.border = "1px dotted black";
-  button.style.margin = "2px";
-  button.style.padding = "2px 5px 2px 5px";
-  button.style.textAlign = "center";
-  button.style.cursor = "pointer";
-}
-
-CategoriesControl.prototype.setSelectedStyle = function(button) {
-  button.style.color = 'black';
-  button.style.fontWeight = 'bold';
-  button.style.cursor = 'default';
-}
-
