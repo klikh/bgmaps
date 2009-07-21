439c2d1baddc831934796d1e26ae6075b5bc8cea
LoKi <Kirill.Likhodedov@gmail.com>
2009-07-21 08:55:31 +0400

    #30. Updated compress.bash. Changed css files and their inclusion into the project to resolve conflicts in the production compressed version, where all css files are linked to every html page

--- tools/compress.bash
+++ tools/compress.bash
@@ -5,16 +5,32 @@ then
   echo "No output dir specified"
   exit 1
 fi
-
 rm -rf $output
 mkdir -p $output
-cp -r index.html event.html changelist.txt jquery-1.3.2.min.js img css results points $output
 
-cat js/*.js >> $output/_script.js
+echo "Copying unmodified files..."
+cp -r index.html event.html changelist.txt img results points $output
 
+echo "Merging js..."
+# and adding newlines between files
+find js -name *.js -exec cat {} \; -exec echo \; > $output/_script.js 
+echo "Merging css..."
+find css -name *.css -exec cat {} \; -exec echo \; > $output/_style.css
+
+echo "Compressing js..."
 java -jar tools/yuicompressor-2.4.2.jar --charset UTF-8 $output/_script.js > $output/script.js
-rm $output/_script.js
+echo "Compressing css..."
+java -jar tools/yuicompressor-2.4.2.jar --charset UTF-8 $output/_style.css > $output/style.css
+rm $output/_script.js $output/_style.css
 
-tools/sedml.sh $output/event.html 's/<!-- JAVASCRIPTS_BEGIN .* JAVASCRIPTS_END -->/  <script src="jquery-1.3.2.min.js" type="text\/javascript"><\/script>\
-  <script src="http:\/\/maps.google.com\/maps\?file=api\&amp\;v=2\&amp\;sensor=false\&amp\;key=ABQIAAAAgviLovAtMLUj4bsG5hDQfhTQtjlvjkD_s50C9vfU31409gl63RSBJPEyRgjnnFAxLpKOM-uD_kHVqg" type="text\/javascript"><\/script>\
+echo "Modifying htmls to link right js and css..."
+
+tools/sedml.sh $output/event.html 's/<!-- JAVASCRIPT_BEGIN .* JAVASCRIPT_END -->/  <script src="http:\/\/maps.google.com\/maps\?file=api\&amp\;v=2\&amp\;sensor=false\&amp\;key=ABQIAAAAgviLovAtMLUj4bsG5hDQfhTQtjlvjkD_s50C9vfU31409gl63RSBJPEyRgjnnFAxLpKOM-uD_kHVqg" type="text\/javascript"><\/script>\
   <script src="script.js" type="text\/javascript"><\/script>/'
+  
+for html in `ls $output/*.html` 
+do
+  tools/sedml.sh $html 's/<!-- CSS_BEGIN .* CSS_END -->/  <link href="style.css" rel="stylesheet" type="text\/css" \/>/'
+done
+
+echo "Project is ready for upload."
