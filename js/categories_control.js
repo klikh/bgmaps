e4074e3ae45b9d6ecd03afe5c94a32fa7931889e
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2012-05-12 20:00:48 +0400

    WIP: Make better cat-control buttons
    Separate map controls and controls in the checkpoint list.
    Use a separate ymaps.control.Button for each control.

--- js/categories_control.js
+++ js/categories_control.js
@@ -10,13 +10,23 @@ function CategoriesControl(categories) {
   this.currentCategory = 'all';
   this.controlPanels = [];
 
-  this.control = new ymaps.control.Button({
-    data : {
-      content : this.makeControlPanel().context    // using 'context' because we need DOM element, not JQuery
-    }
-  });
-
+  this.control = new ymaps.control.ToolBar(this.asYmapsToolButtons());
 }
+
+CategoriesControl.prototype.asYmapsToolButtons = function() {
+  var elements = this._createElements(true);
+  var buttons = [];
+  for (var i = 0; i < elements.length; i++) {
+    buttons.push(new ymaps.control.Button( {
+      data: {
+        content: elements[i].context,
+      }
+    }));
+  }
+  console.log('registering container for elements', elements)
+  this._makeContainerAndRegisterPanel(elements);
+  return buttons;
+};
 
 /**
  * Creates a control panel to filter checkpoints and returns it as a JQuery object
@@ -25,19 +35,8 @@ function CategoriesControl(categories) {
  * @return JQuery holding a DIV with the control panel.
  */
 CategoriesControl.prototype.makeControlPanel = function() {
-  var container = new$('div');
-  
-  this.makeCategoryElement('Все', 'all')  // separately for 'all' pseudo-category
-    .appendTo(container)
-    .addClass('catControlSelectedButton');
-  
-  // then for all other real categories.
-  for (var i = 0; i < this.categories.length; i++) {
-    this.makeCategoryElement(this.categories[i].name, this.categories[i].key).appendTo(container);
-  }
-  
-  this.controlPanels.push(container); // we can have multiple control panels
-  return container;
+  var elements = this._createElements(false);
+  return this._makeContainerAndRegisterPanel(elements);
 };
 
 /**
@@ -64,13 +63,33 @@ CategoriesControl.prototype.selectCategory = function(key) {
 
 /************************************** PRIVATE METHODS ******************************************/
 
+CategoriesControl.prototype._makeContainerAndRegisterPanel = function(elements) {
+  var container = new$('div');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].appendTo(container)
+  }
+  this.controlPanels.push(container); // we can have multiple control panels
+  console.log(this.controlPanels);
+  return container;
+};
+
+
+CategoriesControl.prototype._createElements = function(onMap) {
+  var elements = [];
+  elements.push(this._createElement('Все', 'all', onMap));  // separately for 'all' pseudo-category
+  for (var i = 0; i < this.categories.length; i++) {
+    elements.push(this._createElement(this.categories[i].name, this.categories[i].key, onMap));
+  }
+  return elements;
+};
+
 /**
  * Creates a single SPAN JQuery object holding button to filter by the category it represents.
  * @param {String} name of the category, which will be displayed on the control.
  * @param {String} key to the category, which identifies this category in the 'database'.
  * @return {JQuery} SPAN with the button.
  */
-CategoriesControl.prototype.makeCategoryElement = function(name, key) {
+CategoriesControl.prototype._createElement = function(name, key, onMap) {
   var tooltipText;
   if (key == 'all') {
     tooltipText = 'Показать все категории';
@@ -78,13 +97,16 @@ CategoriesControl.prototype.makeCategoryElement = function(name, key) {
     tooltipText = 'Показать только КП категории «' + name + '»'
   }
   
-  var button = new$('a')
-    .attr('href', '#')
-    .addClass('tooltip')
-    .addClass('catControlButton')
-    .html('<span class="text">' + name + '<img src="img/' + key + '16.png"/></span>' + '<span class="tooltip">' + tooltipText + '</span>')
-    .attr('id', 'catCPButton_' + key);
-  
+  var nameWithImg = '<span class="text">' + name + '<img src="img/' + key + '16.png"/></span>';
+  var button;
+    button = new$('a')
+      .attr('href', '#')
+      .addClass('tooltip')
+      .addClass('catControlButton')
+      .addClass(onMap ? 'mapCatControlButton' : 'pageCatControlButton')
+      .html(nameWithImg + '<span class="tooltip">' + tooltipText + '</span>')
+      .attr('id', 'catCPButton_' + key);
+
   var outer = this;
   button.click(function() { outer.selectCategory(key); return false; });
   return button;
